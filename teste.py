ea2d63c092672684a75234d720eb53b0fda17b68
Renan Greca <renangreca@gmail.com>
2015-01-06 17:39:50 -0200

    teste síncrono

--- teste.py
+++ teste.py
@@ -5,25 +5,32 @@ import argparse
 import socket
 import sys
 import threading
+from multiprocessing import Process
 from socklib import Server
 from socklib import Client
 
-#parser = argparse.ArgumentParser(description='Sends some data back and forth.')
-#parser.add_argument('-n', '--hostname', type=str, required=True,
-#                   help='Network name of the other computer')
+parser = argparse.ArgumentParser(description='Sends some data back and forth.')
+parser.add_argument('-n', '--hostname', type=str, required=True,
+                   help='Network name of the other computer')
 
-#args = parser.parse_args()
-
+args = parser.parse_args()
 
 """
 An echo client that allows the user to send multiple lines to the server.
 Entering a blank line will exit the client.
 """
-
 server = Server()
-server.run()
+proc = Process(target=server.run)
+#server.run()
+proc.start()
 
-host = 'localhost'
+hostname = args.hostname
+if hostname.endswith('.local'):
+    host = socket.gethostbyname(hostname)
+else:
+    host = socket.gethostbyname(hostname+'.local')
+
+#host = 'localhost'
 port = 50000
 size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -39,4 +46,6 @@ while 1:
     data = s.recv(size)
     sys.stdout.write(data)
     sys.stdout.write('%')
+
+proc.join()
 s.close()
