64513e19e1afbaefcdda03e78a525acf8ffbffe3
Renan Greca <renangreca@gmail.com>
2016-01-14 08:01:32 -0200

    final commit

--- generate_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in {1..1000}
-do
-   touch "test$i"
-   truncate -s 1M "test$i"
-done
--- install.sh
+++
@@ -1,9 +0,0 @@
-apt-get install python-dev
-apt-get install python-pip
-wget https://pypi.python.org/packages/source/p/pysendfile/pysendfile-2.0.1.tar.gz
-tar -zxvf pysendfile-2.0.1.tar.gz
-cd pysendfile-2.0.1
-make
-cd ..
-python pysendfile-2.0.1/setup.py build
-python pysendfile-2.0.1/setup.py install
